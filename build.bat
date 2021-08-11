ce858f2e785351198bd5a5048830601587cf7fc3
Found4k <88767692+Found4k@users.noreply.github.com>
2021-08-11 18:02:23 +1000

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,41 @@
+@echo off
+echo Hello Welcome to the BUILD INFO tool of StickNapped.
+PAUSE
+@echo off
+echo  To build this file open the StickNapped SOURCE_openwithPIVIT.piv with PIVIT
+@PAUSE
+@echo off
+echo Once you have done this click file
+PAUSE
+@echo off
+echo And select Export Animation
+PAUSE
+@echo off
+echo Export it as a .gif file
+PAUSE
+@echo off
+echo Now go to freeconvert.com and select .gif to .mp4
+PAUSE
+@echo off
+echo Wait for it to convert.
+PAUSE
+@echo off
+echo EXPORT 1: Export to youtube
+PAUSE
+@echo off
+echo Go to youtube.com
+PAUSE
+@echo off
+echo Select the camera with a plus
+PAUSE
+@echo off
+echo Select upload
+PAUSE
+@echo off
+echo Now select the files
+PAUSE
+@echo off
+echo And then fill in all the fields, title, description, icon ect,
+PAUSE
+@ehco off
+echo Now your export should be complete
